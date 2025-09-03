744d6d43759adb2e1d5a856e7b6e4bd0b3b60312
Phone Pyae Naing <murasakinaing@gmail.com>
2025-09-03 21:24:17 +0630

    Update Constants.kt
    Update

--- app/src/main/java/com/example/moviemate/shared/utils/Constants.kt
+++ app/src/main/java/com/example/moviemate/shared/utils/Constants.kt
@@ -2,7 +2,7 @@ package com.example.moviemate.shared.utils
 
 object Constants {
 
-    const val API_KEY = "d29ff4d28b1733c6b0155bfdad2a20c9"
+    const val API_KEY = ""
     const val IMAGE_BASE_PATH ="https://image.tmdb.org/t/p/w500/"
 
     enum class Mode {Browse, Search}
